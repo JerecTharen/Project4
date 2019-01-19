9fdc81e909a506594934207d3fb0c11d7f156596
Jacob Bennett <kf7ags@gmail.com>
2019-01-19 16:50:14 -0700

    course info now shows on scorecard

--- src/app/scorecard/score-api.service.ts
+++ src/app/scorecard/score-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,19 @@ export class ScoreAPIService {
 
   private courseURL: string = 'https://golf-courses-api.herokuapp.com/courses/';
   private courseID: string;
-  constructor(private httpC: HttpClient) { }
+  constructor(
+    private httpC: HttpClient,
+    private route: ActivatedRoute
+  ) { }
 
-  getCourse(): Observable<any>{
-    return this.httpC.get<any>(this.courseURL + this.courseID);
+  getCourse(devLoad?: string): Observable<any>{
+    if(!devLoad){
+      return this.httpC.get<any>(this.courseURL + this.courseID);
+    }
+    else{
+      this.courseID = devLoad;
+      return this.httpC.get<any>(this.courseURL + this.courseID);
+    }
   }
   updateCourseID(id: string): void{
     this.courseID = id;
--- src/app/scorecard/scorecard.component.ts
+++ src/app/scorecard/scorecard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ScoreAPIService} from "./score-api.service";
 import {StartServiceService} from "../start/start-service.service";
+import {ActivatedRoute} from "@angular/router";
 
 const testData: any[] = [
   {hole: 1, names: ['jacob', 'jerec', 'aralin'], scores: [1,2,3]},
@@ -16,9 +17,14 @@ const testData: any[] = [
 })
 export class ScorecardComponent implements OnInit {
   private courseInfo: any[];
+  private selectedTee: number;
+  private courseYardage: string[] = ['--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--'];
+  private courseHandicap: string[] = ['--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--'];
+  private coursePAR: string[] = ['--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--', '--'];
   constructor(
     private scoreAPI: ScoreAPIService,
-    private startService: StartServiceService
+    private startService: StartServiceService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
@@ -28,9 +34,37 @@ export class ScorecardComponent implements OnInit {
 
   getCourseInfo(): void{
     this.scoreAPI.getCourse().subscribe(info => {
-      this.courseInfo = info.data;
+      this.courseInfo = info.data.holes;
       console.log(this.courseInfo);
     });
+    if(this.courseInfo === undefined){
+      this.scoreAPI.getCourse(this.route.snapshot.paramMap.get('id')).subscribe(info => {
+        this.courseInfo = info.data.holes;
+        console.log(this.courseInfo);
+      });
+    }
+  }
+
+  sayTeeType(): void{
+    console.log(this.selectedTee);
+  }
+
+  readyInfo(tee: number): void{
+    this.courseYardage = [];
+    this.courseHandicap = [];
+    this.coursePAR = [];
+    for(let i: number=0; i < 18; i++){
+      // let holeNum: string = `hole${i+1}`;
+      // console.log(holeNum);
+      this.courseYardage.push(this.courseInfo[i].teeBoxes[tee-1].yards);
+    }
+    // console.log(this.courseYardage);
+    for(let y: number = 0; y < 18; y++){
+      this.courseHandicap.push(this.courseInfo[y].teeBoxes[tee-1].hcp);
+    }
+    for(let x: number = 0; x < 18; x++){
+      this.coursePAR.push(this.courseInfo[x].teeBoxes[tee-1].par);
+    }
   }
 
   DATASOURCE = testData;
